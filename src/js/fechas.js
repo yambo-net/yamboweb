ad3067a9d8c84002f34a3ee4f35b9f778ad55a68
Álvaro Tobar <atobar@buk.cl>
2026-06-27 12:36:05 -0400

    fix(fechas): fix hover behaviour on mobile devices

--- src/js/fechas.js
+++ src/js/fechas.js
@@ -1,29 +1,44 @@
 let attached = false;
-
+let isTouchDevice = 'ontouchstart' in document.documentElement;
 let imageContainer = document.querySelector('#floating-image-container');
+let imageUrl = null;
+
+if (isTouchDevice) {
+  imageContainer.removeEventListener('mouseenter', showImage);
+  imageContainer.removeEventListener('mouseleave', hideImage);
+}
 
 const followMouse = (event) => {
   imageContainer.style.left = event.x + 'px';
   imageContainer.style.top = event.y + 'px';
 };
 
-async function showImage(imageUrl) {
-  if (!attached) {
+function showImage(newImageUrl) {
+  console.log('show');
+  if (!attached || newImageUrl != imageUrl) {
+    console.log('inside');
     attached = true;
-
+    imageUrl = newImageUrl;
     let composedUrl = `/assets/flyers/${imageUrl}.png`;
     imageContainer.children[0].src = composedUrl;
     imageContainer.style.display = 'block';
-    imageContainer.style.left = event.x + 'px';
+    if (!isTouchDevice) {
+      imageContainer.style.left = event.x + 'px';
+      document.addEventListener('pointermove', followMouse);
+    } else {
+      imageContainer.style.right = '1rem';
+    }
     imageContainer.style.top = event.y + 'px';
-    document.addEventListener('pointermove', followMouse);
-    document.addEventListener('touchmove', followMouse);
   }
 }
 
 function hideImage() {
   attached = false;
   imageContainer.style.display = 'none';
-  document.removeEventListener('pointermove', followMouse);
-  document.removeEventListener('touchmove', followMouse);
+  if (!isTouchDevice) document.removeEventListener('pointermove', followMouse);
+}
+
+function toggleImage(newImageUrl) {
+  console.log('toggle');
+  attached && imageUrl == newImageUrl ? hideImage() : showImage(newImageUrl);
 }
